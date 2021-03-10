4bec557ed4e50cc22677b644bf642255c9ca1017
luizsac <lu_que90@hotmail.com>
2021-03-09 22:36:27 -0300

    Exercício da aula 'O que é List'

--- src/main/java/one/digitalinnovation/list/ListaPrograma.java
+++ src/main/java/one/digitalinnovation/list/ListaPrograma.java
@@ -1,7 +1,48 @@
 package one.digitalinnovation.list;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ListaPrograma {
 
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
 
+        lista.add("Juliana");
+        lista.add("Pedro");
+        lista.add("Carlos");
+        lista.add("Larissa");
+        lista.add("João");
+
+        for (String nome: lista) {
+            System.out.println(nome);
+        }
+
+        int index = lista.indexOf("Carlos");
+        lista.set(index, "Roberto");
+
+        System.out.println(lista.get(1));
+        System.out.println(lista.get(4));
+
+        lista.remove("João");
+
+        System.out.println("Tamanho da lista: " + lista.size());
+        System.out.println("Juliano está na lista: " + lista.contains("Juliano"));
+
+        List<String> lista2 = new ArrayList<>();
+
+        lista2.add("Ismael");
+        lista2.add("Rodrigo");
+
+        lista.addAll(lista2);
+
+        System.out.println(lista);
+
+        Collections.sort(lista);
+
+        System.out.println(lista);
+        System.out.println("Lista está vazia: " + lista.isEmpty());
+    }
 
 }
